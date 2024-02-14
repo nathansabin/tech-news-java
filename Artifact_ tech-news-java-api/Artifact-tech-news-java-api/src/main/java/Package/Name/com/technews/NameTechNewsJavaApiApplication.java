37f2ecb0cccca207bf82ae477f82decd354e62e7
nathansabin <sabinnat001@gmail.com>
2024-02-13 22:22:04 -0800

    init

---
+++ Artifact_ tech-news-java-api/Artifact-tech-news-java-api/src/main/java/Package/Name/com/technews/NameTechNewsJavaApiApplication.java
@@ -0,0 +1,13 @@
+package Package.Name.com.technews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NameTechNewsJavaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NameTechNewsJavaApiApplication.class, args);
+	}
+
+}
---
+++ Artifact_ tech-news-java-api/Artifact-tech-news-java-api/src/test/java/Package/Name/com/technews/NameTechNewsJavaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package Package.Name.com.technews;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NameTechNewsJavaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
