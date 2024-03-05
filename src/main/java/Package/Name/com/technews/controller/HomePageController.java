5501cfb207ec6c9398d31b7df764c52f8e67616a
nathansabin <sabinnat001@gmail.com>
2024-03-04 21:24:15 -0800

    made comment, delete and post function

--- src/main/java/Package/Name/com/technews/controller/HomePageController.java
+++ src/main/java/Package/Name/com/technews/controller/HomePageController.java
@@ -42,66 +42,7 @@ public class HomePageController {
         model.addAttribute("user", new User());
         return "login";
     }
-    @PostMapping("/users/login")
-    public String login(@ModelAttribute User user, Model model, HttpServletRequest request) throws Exception {
 
-        if ((user.getPassword().equals(null) || user.getPassword().isEmpty()) || (user.getEmail().equals(null) || user.getPassword().isEmpty())) {
-            model.addAttribute("notice", "Email address and password must be populated in order to login!");
-            return "login";
-        }
-
-        User sessionUser = userRepository.findUserByEmail(user.getEmail());
-
-        try {
-            // If sessionUser is invalid, running .equals() will throw an error
-            if (sessionUser.equals(null)) {
-
-            }
-            // We will catch an error and notify client that email address is not recognized
-        } catch (NullPointerException e) {
-            model.addAttribute("notice", "Email address is not recognized!");
-            return "login";
-        }
-
-        // Validate Password
-        String sessionUserPassword = sessionUser.getPassword();
-        boolean isPasswordValid = BCrypt.checkpw(user.getPassword(), sessionUserPassword);
-        if(isPasswordValid == false) {
-            model.addAttribute("notice", "Password is not valid!");
-            return "login";
-        }
-
-        sessionUser.setLoggedIn(true);
-        request.getSession().setAttribute("SESSION_USER", sessionUser);
-
-        return "redirect:/dashboard";
-    }
-    @PostMapping("/users")
-    public String signup(@ModelAttribute User user, Model model, HttpServletRequest request) throws Exception {
-        if ((user.getUsername().equals(null) || user.getUsername().isEmpty()) || (user.getPassword().equals(null) || user.getPassword().isEmpty()) || (user.getEmail().equals(null) || user.getPassword().isEmpty())) {
-            model.addAttribute("notice", "In order to signup username, email address and password must be populated!");
-            return "login";
-        }
-        try {
-            // Encrypt password
-            user.setPassword(BCrypt.hashpw(user.getPassword(), BCrypt.gensalt()));
-            userRepository.save(user);
-        } catch (DataIntegrityViolationException e) {
-            model.addAttribute("notice", "Email address is not available! Please choose a different unique email address.");
-            return "login";
-        }
-        User sessionUser = userRepository.findUserByEmail(user.getEmail());
-        try {
-            if (sessionUser.equals(null)) {
-            }
-        } catch (NullPointerException e) {
-            model.addAttribute("notice", "User is not recognized!");
-            return "login";
-        }
-        sessionUser.setLoggedIn(true);
-        request.getSession().setAttribute("SESSION_USER", sessionUser);
-        return "redirect:/dashboard";
-    }
 
     @GetMapping("/users/logout")
     public String logout(HttpServletRequest request)
--- src/main/resources/static/js/delete-post.js
+++ src/main/resources/static/js/delete-post.js
@@ -10,7 +10,8 @@ async function deleteFormHandler(event) {
   });
 
   if (response.ok) {
-    document.location.replace('/dashboard/')
+    console.log("changed")
+    document.location.replace('/dashboard');
   } else {
     alert(response.statusText);
   }
